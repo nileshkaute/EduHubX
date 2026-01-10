1c8f6a11307f2ec89d1d40c8ce4313f8d09f63ff
Nilesh Kaute <nileshkaute1@gmail.com>
2026-01-10 11:56:53 +0530

    feat(backend): initialize express server with mongodb and user model

---
+++ backend/models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      // Password is not required if logging in via Google
+    },
+    photo: {
+      type: String,
+    },
+    googleId: {
+      type: String,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
